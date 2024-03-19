cd7fa0553ac9cab9d8eaad4d667aaf85c4aac474
김진영 <162416743+qextory-admin@users.noreply.github.com>
2024-03-19 22:14:34 +0900

    feat: 프리텐다드 폰트 설정

--- src/pages/auth/ui/SignInPage.tsx
+++ src/pages/auth/ui/SignInPage.tsx
@@ -3,6 +3,7 @@
 import { styled } from 'styled-components';
 
 import { LoginForm } from '@/features/auth/ui';
+import Logo from '@/shared/assets/images/logo.svg';
 
 import { AuthLayout } from './AuthLayout';
 
@@ -12,13 +13,16 @@ export const Container = styled.div`
   align-items: center;
   width: 100%;
   padding: 30px 20px;
+  font-size: 30px;
+  font-weight: 500;
 `;
 
 export const SignInPage = () => {
   return (
     <AuthLayout>
       <Container>
-        <h2>로그인</h2>
+        <h2>프리텐다드</h2>
+        <Logo />
         <LoginForm />
       </Container>
     </AuthLayout>
