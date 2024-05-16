e695655e429b0d099f2514090a55921839eb3e0c
hyemin065 <p62517@gmail.com>
2024-05-16 22:31:23 +0900

    fix: 수강권 내역 타입 수정, 수강권 컴포넌트 prop 수정

--- src/feature/member/api/useStudentCourseDetailQuery.ts
+++ src/feature/member/api/useStudentCourseDetailQuery.ts
@@ -3,23 +3,7 @@ import { useInfiniteQuery } from '@tanstack/react-query';
 import { authApi } from '@/entity/auth';
 import { BaseError, BaseResponse } from '@/shared/api';
 
-import { courseHistoryCodeDescription } from '../const';
-import { CourseItem } from '../model/types';
-
-type CourseHistory = keyof typeof courseHistoryCodeDescription;
-
-interface CourseHistoryItem {
-  courseHistoryId: number;
-  cnt: number;
-  calculation: string;
-  type: CourseHistory;
-  createdAt: string;
-}
-
-interface StudentCourse {
-  course: CourseItem;
-  courseHistories: CourseHistoryItem[];
-}
+import { StudentCourse } from '../model/types';
 
 export const useStudentCourseDetailQuery = (memberId: number, size: number) => {
   return useInfiniteQuery<StudentCourse, BaseError>({
--- src/feature/member/ui/CourseCard.tsx
+++ src/feature/member/ui/CourseCard.tsx
@@ -2,6 +2,7 @@
 
 import { HTMLAttributes, ReactNode } from 'react';
 
+import { Typography } from '@/shared/mixin';
 import { cn } from '@/shared/utils';
 
 import { Card, CardContent, CardHeader } from '../../../shared/ui/card';
@@ -26,31 +27,24 @@ interface CourseCardHeaderProps {
   remainLessonCnt?: number;
   totalLessonCnt?: number;
   expiration?: boolean;
-  title?: string;
-  indication?: string;
 }
 
 export const CourseCardHeader = ({
   gymName,
   remainLessonCnt,
-  title,
-  indication,
+  totalLessonCnt,
+  expiration,
 }: CourseCardHeaderProps) => (
   <CardHeader>
-    {gymName ? (
-      <>
-        <div className='typography-body-3 mb-1 flex items-center justify-between text-[#fff]'>
-          <p>{gymName}</p>
-          <span>{title}</span>
-        </div>
-        <p className='typography-heading-3'>{remainLessonCnt}회 남아있어요!</p>
-      </>
-    ) : (
-      <div className='flex items-center justify-between'>
-        <p className='typography-heading-4 text-[#fff]'>{title}</p>
-        <span className='typography-body-3 text-[#fff]'>{indication}</span>
-      </div>
-    )}
+    <div className='typography-body-3 mb-1 flex items-center justify-between text-white'>
+      <p className={cn(Typography.BODY_3, 'text-gray-100')}>{gymName}</p>
+      <span className={cn(Typography.BODY_3, 'text-gray-100')}>
+        {`PT ${totalLessonCnt}회 수강권`}
+      </span>
+    </div>
+    <p className={cn(Typography.HEADING_3, 'text-white')}>
+      {expiration ? `${10}회 PT수강 만료` : `${remainLessonCnt}`}회 남아있어요!
+    </p>
   </CardHeader>
 );
 
@@ -72,13 +66,13 @@ export const CourseCardContent = ({
       <p className='typography-heading-5 mb-[6px] text-[#fff]'>
         PT 진행 횟수 {remainLessonCnt}
         <span className={cn('typography-body-3 text-[#8EC7FF]', className)}>
-          / {totalLessonCnt}
+          /{totalLessonCnt}
         </span>
       </p>
       <Progress
         className='h-[2px]'
         progressClassName={progressClassName}
-        value={((totalLessonCnt - remainLessonCnt) / totalLessonCnt) * 100}
+        value={(remainLessonCnt / totalLessonCnt) * 100}
       />
     </div>
   </CardContent>
