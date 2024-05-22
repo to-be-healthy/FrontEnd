e28cb6b2659cc40e1e93091469436679b20b26fa
김진영 <skswls0@daum.net>
2024-05-22 17:36:06 +0900

    feat: 온보딩 페이지 로그인 여부 판별

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,27 @@
+'use client';
+
+import { redirect } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
+import { auth } from '@/entity/auth';
 import { OnboardingPage } from '@/page/auth';
 
 export default function Page() {
-  return <OnboardingPage />;
+  const [role, setRole] = useState<string | null>();
+
+  useEffect(() => {
+    const { memberType } = auth();
+    const newRole = memberType ?? null;
+    setRole(newRole);
+  }, [role]);
+
+  if (role === undefined) {
+    return '로딩중';
+  }
+
+  if (role === null) {
+    return <OnboardingPage />;
+  }
+
+  return redirect(`/${role?.toLowerCase()}`);
 }
