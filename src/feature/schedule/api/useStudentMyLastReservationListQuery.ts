c392057fc5857a2b2de1dc231706795cbca743f9
hyemin065 <p62517@gmail.com>
2024-06-10 19:18:04 +0900

    fix: 수업예약 api 추가 및 수정

--- src/feature/schedule/api/useStudentMyLastReservationListQuery.ts
+++ src/feature/schedule/api/useStudentMyLastReservationListQuery.ts
@@ -5,12 +5,12 @@ import { BaseError, BaseResponse } from '@/shared/api';
 
 import { MyReservationResponse } from '../model/type';
 
-export const useStudentMyReservationListQuery = () => {
+export const useStudentMyLastReservationListQuery = (searchDate: string) => {
   return useQuery<MyReservationResponse, BaseError>({
-    queryKey: ['StudentMyReservationList'],
+    queryKey: ['StudentMyLastReservationList'],
     queryFn: async () => {
       const res = await authApi.get<BaseResponse<MyReservationResponse>>(
-        `/api/schedule/v1/student/my-reservation/new`
+        `/api/schedule/v1/student/my-reservation/old?searchDate=${searchDate}`
       );
       return res.data.data;
     },
