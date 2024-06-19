627351b5dea003d73eaab172a8a2df8be7f533c5
김진영 <skswls0@daum.net>
2024-06-19 19:18:50 +0900

    hotfix: 버튼 전역 스타일링 제거

--- src/shared/ui/button.tsx
+++ src/shared/ui/button.tsx
@@ -4,8 +4,6 @@ import * as React from 'react';
 
 import { cn } from '@/shared/utils/tw-utils';
 
-import { Typography } from '../mixin';
-
 const buttonVariants = cva(
   'inline-flex items-center justify-center whitespace-nowrap rounded-lg text-sm font-medium transition-colors focus-visible:outline-none disabled:pointer-events-none disabled:bg-gray-300',
   {
@@ -45,10 +43,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     const Comp = asChild ? Slot : 'button';
     return (
       <Comp
-        className={cn(
-          buttonVariants({ variant, size, className }),
-          Typography.TITLE_1_BOLD
-        )}
+        className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
       />
