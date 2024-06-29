459f15786b2fa4e403294a9530d9b1f8c67ecbaf
김진영 <skswls0@daum.net>
2024-06-29 14:46:08 +0900

    edit: 운동 종류 다중 선택 시 useRef 중복 사용되는 문제 수정

--- src/page/workout/ui/CreateWorkoutPage.tsx
+++ src/page/workout/ui/CreateWorkoutPage.tsx
@@ -3,7 +3,7 @@
 import { useQueryClient } from '@tanstack/react-query';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
-import { useRef, useState } from 'react';
+import { createRef, RefObject, useRef, useState } from 'react';
 
 import {
   AppendNewExerciseType,
@@ -38,17 +38,23 @@ const CreateWorkoutPage = () => {
   const [viewMySelf, setViewMySelf] = useState(true);
   const [completedExercises, setCompletedExercises] = useState<ComplexExercise[]>([]);
 
-  const setNumRef = useRef<HTMLInputElement>(null);
-  const numberOfCyclesRef = useRef<HTMLInputElement>(null);
-  const weightRef = useRef<HTMLInputElement>(null);
+  const setNumRef = useRef<RefObject<HTMLInputElement>[]>([]);
+  const numberOfCyclesRef = useRef<RefObject<HTMLInputElement>[]>([]);
+  const weightRef = useRef<RefObject<HTMLInputElement>[]>([]);
 
   const appendExcercise = (newExcerciseTypes: ExerciseType[]) => {
-    const newExcercises = newExcerciseTypes.map((item) => ({
-      ...item,
-      setNum: 0,
-      weight: 0,
-      numberOfCycles: 0,
-    }));
+    const newExcercises = newExcerciseTypes.map((item) => {
+      setNumRef.current.push(createRef<HTMLInputElement>());
+      numberOfCyclesRef.current.push(createRef<HTMLInputElement>());
+      weightRef.current.push(createRef<HTMLInputElement>());
+      return {
+        ...item,
+        setNum: 0,
+        weight: 0,
+        numberOfCycles: 0,
+      };
+    });
+
     setCompletedExercises((prev) => [...prev, ...newExcercises]);
   };
 
@@ -172,7 +178,7 @@ const CreateWorkoutPage = () => {
             <IconPlus width={14} height={14} />
           </Button>
           {completedExercises.length > 0 &&
-            completedExercises.map((completedExcercise) => (
+            completedExercises.map((completedExcercise, index) => (
               <div
                 key={completedExcercise.exerciseId}
                 className='flex w-full flex-col gap-7 rounded-lg border border-gray-200 px-6 py-7'>
@@ -196,14 +202,14 @@ const CreateWorkoutPage = () => {
                         'focus-within:border focus-within:border-primary focus-within:bg-white'
                       )}
                       onClick={() => {
-                        setNumRef.current?.focus();
-                        setNumRef.current?.setSelectionRange(
+                        setNumRef.current[index].current?.focus();
+                        setNumRef.current[index].current?.setSelectionRange(
                           String(completedExcercise.setNum).length,
                           String(completedExcercise.setNum).length
                         );
                       }}>
                       <input
-                        ref={setNumRef}
+                        ref={setNumRef.current[index]}
                         type='text'
                         value={completedExcercise.setNum}
                         pattern='[0-9]*'
@@ -240,14 +246,14 @@ const CreateWorkoutPage = () => {
                         'focus-within:border focus-within:border-primary focus-within:bg-white'
                       )}
                       onClick={() => {
-                        numberOfCyclesRef.current?.focus();
-                        numberOfCyclesRef.current?.setSelectionRange(
+                        numberOfCyclesRef.current[index].current?.focus();
+                        numberOfCyclesRef.current[index].current?.setSelectionRange(
                           String(completedExcercise.numberOfCycles).length,
                           String(completedExcercise.numberOfCycles).length
                         );
                       }}>
                       <input
-                        ref={numberOfCyclesRef}
+                        ref={numberOfCyclesRef.current[index]}
                         type='text'
                         pattern='[0-9]*'
                         inputMode='numeric'
@@ -284,14 +290,14 @@ const CreateWorkoutPage = () => {
                         'focus-within:border focus-within:border-primary focus-within:bg-white'
                       )}
                       onClick={() => {
-                        weightRef.current?.focus();
-                        weightRef.current?.setSelectionRange(
+                        weightRef.current[index].current?.focus();
+                        weightRef.current[index].current?.setSelectionRange(
                           String(completedExcercise.weight).length,
                           String(completedExcercise.weight).length
                         );
                       }}>
                       <input
-                        ref={weightRef}
+                        ref={weightRef.current[index]}
                         type='text'
                         value={completedExcercise.weight}
                         pattern='[0-9]*'
