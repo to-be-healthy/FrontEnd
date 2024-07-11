b04380a25860d1fab194e6895aa6147efe6af9a6
김진영 <skswls0@daum.net>
2024-07-11 13:12:35 +0900

    refactor: provider 컨벤션 통일

--- src/app/_providers/index.ts
+++ src/app/_providers/index.ts
@@ -1,4 +1,5 @@
 export { KakaoScript } from './KakaoScript';
 export { MSWComponent } from './MSWComponent';
 export { QueryProvider } from './QueryProvider';
+export { ToastProvider } from './ToastProvider';
 export { UserRoleMiddleware } from './UserRoleMiddleware';
